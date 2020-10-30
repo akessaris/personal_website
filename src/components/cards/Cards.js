6ef50c19b823519f631be741d3f06b3e562a405b
Alexander Kessaris <ank352@nyu.edu>
2020-10-29 23:02:47 -0400

    setup change handler for arrow click, starting adding subheader to card

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -4,13 +4,26 @@ import './Cards.scss';
 import { cards } from '../../tools/mockDB.json';
 
 const Cards = () => {
-  const [card, setCard] = useState(cards[0]);
+  const [cardIndex, setCardIndex] = useState(0);
 
-  const { _id, title, body, image } = card;
+  const { _id, title, body, image } = cards[cardIndex];
+
+  const handleChange = (direction) => {
+    setCardIndex(prevIndex => {
+      const updatedIndex = direction === 'left' ? (prevIndex - 1) : (prevIndex + 1);
+      return (updatedIndex >= 0) ? updatedIndex : prevIndex;
+    });
+  };
 
   return (
     <div className="container">
-      <Card key={_id} title={title} body={body} image={image}/>
+      <Card
+        key={_id}
+        title={title}
+        body={body}
+        image={image}
+        onArrowClick={handleChange}
+      />
     </div>
   )
 };
--- src/components/cards/card/Card.js
+++ src/components/cards/card/Card.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from "prop-types";
 import './Card.scss';
 
-const Card = ({ title, body, image }) => {
+const Card = ({ title, body, image, onArrowClick }) => {
   return (
     <div className="card-container">
-      <div className="card">
-        <img className="image" src={`src/assets/images/${image}`}/>
+      <div className="card-column">
+        <div className="card">
+          <img className="image" src={`src/assets/images/${image}`}/>
+          <p className="subheader"><span className="name">Alex Kessaris</span>, <span className="age">24</span></p>
+        </div>
         {/* TODO: replace with actual icons */}
         <div className="icon-container">
           <img className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
@@ -18,8 +21,8 @@ const Card = ({ title, body, image }) => {
         <h2>{title}</h2>
         <p>{body}</p>
         <div className="icon-container">
-          <img className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
-          <img className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
+          <img onClick={() => onArrowClick('left') } className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
+          <img onClick={() => onArrowClick('right') } className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
         </div>
       </div>
     </div>
@@ -30,6 +33,7 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  onArrowClick: PropTypes.func.isRequired,
 }
 
 export default Card;
