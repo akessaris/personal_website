a1c717ea7516d71687cdf89721f7b414017b26d5
Alexander Kessaris <ank352@nyu.edu>
2020-10-29 22:31:29 -0400

    styled icons with cards

--- src/components/cards/card/Card.js
+++ src/components/cards/card/Card.js
@@ -4,13 +4,23 @@ import './Card.scss';
 
 const Card = ({ title, body, image }) => {
   return (
-    <div className="card">
-      <div className="column">
-        <img className="image" src={`src/assets/${image}`} />
-        <span className="text">
-          <h2>{title}</h2>
-          <p>{body}</p>
-        </span>
+    <div className="card-container">
+      <div className="card">
+        <img className="image" src={`src/assets/images/${image}`}/>
+        {/* TODO: replace with actual icons */}
+        <div className="icon-container">
+          <img className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
+          <img className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
+        </div>
+      </div>
+
+      <div className="text">
+        <h2>{title}</h2>
+        <p>{body}</p>
+        <div className="icon-container">
+          <img className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
+          <img className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
+        </div>
       </div>
     </div>
   );
