ca1ea5b5f35eaf4d0ac60b8cc66c9739a264dbe3
Alexander Kessaris <ank352@nyu.edu>
2020-11-03 17:28:31 -0500

    moved card functionality outside to parent container, add material icons

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,28 +2,39 @@ import React, { useState } from 'react';
 import Card from './card/Card';
 import './Cards.scss';
 import { cards } from '../../tools/mockDB.json';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ClearIcon from '@material-ui/icons/Clear';
+import { SvgIcon } from '@material-ui/core';
 
 const Cards = () => {
   const [cardIndex, setCardIndex] = useState(0);
 
-  const { _id, title, body, image } = cards[cardIndex];
-
-  const handleChange = (direction) => {
-    setCardIndex(prevIndex => {
-      const updatedIndex = direction === 'left' ? (prevIndex - 1) : (prevIndex + 1);
-      return (updatedIndex >= 0) ? updatedIndex : prevIndex;
-    });
-  };
+  const { _id, title, image } = cards[cardIndex];
 
   return (
     <div className="container">
-      <Card
-        key={_id}
-        title={title}
-        body={body}
-        image={image}
-        onArrowClick={handleChange}
-      />
+      <div className="card-column">
+        <div className="card">
+          <Card
+            key={_id}
+            title={title}
+            image={image}
+          />
+          <p className="subheader"><span className="name">Alex Kessaris</span>, <span className="age">24</span></p>
+          </div>
+
+          <div className="icon-container">
+            <SvgIcon class="icon clear"><ClearIcon/></SvgIcon>
+            <SvgIcon class="icon heart"><FavoriteIcon/></SvgIcon>
+          </div>
+      </div>
+
+      <div className="text">
+        <h1>Hello there</h1>
+        <p>
+          Testing testing 123
+        </p>
+      </div>
     </div>
   )
 };
--- src/components/cards/card/Card.js
+++ src/components/cards/card/Card.js
@@ -2,38 +2,15 @@ import React from 'react';
 import PropTypes from "prop-types";
 import './Card.scss';
 
-const Card = ({ title, body, image, onArrowClick }) => {
+const Card = ({ title, image }) => {
   return (
-    <div className="card-container">
-      <div className="card-column">
-        <div className="card">
-          <img className="image" src={`src/assets/images/${image}`}/>
-          <p className="subheader"><span className="name">Alex Kessaris</span>, <span className="age">24</span></p>
-        </div>
-        {/* TODO: replace with actual icons */}
-        <div className="icon-container">
-          <img className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
-          <img className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
-        </div>
-      </div>
-
-      <div className="text">
-        <h2>{title}</h2>
-        <p>{body}</p>
-        <div className="icon-container">
-          <img onClick={() => onArrowClick('left') } className="icon-arrow left" src="src/assets/icons/left-arrow.svg"/>
-          <img onClick={() => onArrowClick('right') } className="icon-arrow" src="src/assets/icons/right-arrow.svg"/>
-        </div>
-      </div>
-    </div>
+    <img className="image" src={`src/assets/images/${image}`} alt={title}/>
   );
 };
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  onArrowClick: PropTypes.func.isRequired,
 }
 
 export default Card;
